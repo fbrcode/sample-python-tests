709059fb8066008ad1a1aba3db50510b4fb90f94
fbrcode <fabio.bressler@gmail.com>
2022-12-06 14:00:28 -0300

    reorg startup application

--- src/foo.py
+++ src/foo.py
@@ -3,3 +3,11 @@ def is_a_bigger(a, b):
         return True
     elif a < b:
         return False
+
+
+def main():
+    print(is_a_bigger(2, 1))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.foo import is_a_bigger
+
+
+def main():
+    print(is_a_bigger(2, 1))
+
+
+if __name__ == "__main__":
+    main()
