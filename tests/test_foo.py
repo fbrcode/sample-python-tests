84febc89f66d2544be6b3ed2ca296a76ff12453e
fbrcode <fabio.bressler@gmail.com>
2022-12-06 16:29:10 -0300

    make tests pass

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,22 +1,22 @@
 from src.foo import is_a_bigger
 
 
-# def test_is_a_bigger():
-#     a = 2
-#     b = 1
-#     answer = is_a_bigger(a, b)
-#     assert answer
+def test_is_a_bigger():
+    a = 2
+    b = 1
+    answer = is_a_bigger(a, b)
+    assert answer
 
 
-# def test_invalid_is_a_bigger():
-#     a = 1
-#     b = 2
-#     answer = is_a_bigger(a, b)
-#     assert not answer
+def test_invalid_is_a_bigger():
+    a = 1
+    b = 2
+    answer = is_a_bigger(a, b)
+    assert not answer
 
 
-# def test_is_equal():
-#     a = 1
-#     b = 1
-#     answer = is_a_bigger(a, b)
-#     assert answer is None
+def test_is_equal():
+    a = 1
+    b = 1
+    answer = is_a_bigger(a, b)
+    assert answer is None
