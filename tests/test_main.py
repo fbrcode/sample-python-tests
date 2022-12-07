f1a1d73e5aeecac0511358b2d1e5b1e60e932b59
fbrcode <fabio.bressler@gmail.com>
2022-12-07 20:23:38 -0300

    fix unused variable

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,7 @@ from app.main import main
 
 def test_main(capfd):
     main()  # print to stdout
-    out, err = capfd.readouterr()
+    (out, err) = capfd.readouterr()
+    if err != "":
+        print(err)
     assert out == "Log level is INFO\nTrue\n"
